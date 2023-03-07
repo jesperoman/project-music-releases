aa46b0218fca5640003f7f9906a6d409b304f87c
Jesper Öman <jesper.oman@magine.com>
2023-03-07 17:15:18 +0100

    Add album component, iterate over album items

---
+++ code/src/Album.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Album = (props) => {
+  return (<h1>{props.item.name}</h1>)
+}
--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import data from './data.json';
+import { Album } from './Album.js';
 
 console.log(data);
+
+const albumItems = data.albums.items.map((item, index) => {
+  item.key = index;
+  return item;
+});
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      {
+        albumItems.map((item) => (
+          <Album item={item} key={item.key} />
+        ))
+      }
     </div>
-  );
+  )
 }
