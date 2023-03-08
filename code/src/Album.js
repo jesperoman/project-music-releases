a2186fda1c92ee2b02efe36fddb83232ccbc7c09
jenster5 <120317167+jenster5@users.noreply.github.com>
2023-03-08 11:27:21 +0100

    adding artist name,url, and icons

--- code/src/Album.js
+++ code/src/Album.js
@@ -1,13 +1,36 @@
 import React from 'react';
 
 export const Album = (props) => {
-  return (
+  const artists = props.item.artists.map((artist) => ({
+    name: artist.name,
+    url: artist.external_urls.spotify
+  }));
+  const artistLinks = artists.map((artist) => (
+    <a key={artist.url} href={artist.url} target="_blank" rel="noopener noreferrer">{artist.name}</a>
+  ));
 
-    <div className="rubrik">
-      <h1>{props.item.name}</h1>
-      <a href={props.item.external_urls.spotify}>
-        <img src={props.item.images.find((image) => image.width === 300).url} alt="album cover" />
-      </a>
+  return (
+    <div className="body">
+
+      <div className="image-container">
+        <a href={props.item.external_urls.spotify} target="_blank" rel="noopener noreferrer">
+          <img className="album-image" src={props.item.images.find((image) => image.width === 300).url} alt="album cover" />
+        </a>
+        <div className="icons-container">
+          <img src="./icons/heart.svg" className="heart-icon" alt="heart icon" />
+          <img src="./icons/play.svg" className="play-icon" alt="play icon" />
+          <img src="./icons/dots.svg" className="dots-icon" alt="dots icon" />
+        </div>
+      </div>
+
+      <div className="album-name">
+        <h1>{props.item.name}</h1>
+      </div>
+
+      <div className="artist-name">
+        <p>{artistLinks}</p>
+      </div>
+
     </div>
   )
 }
